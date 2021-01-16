3cae8d802fc3e7e58720c3b764a74f98d904b59d
Marcos Gaspar <Marga5@users.noreply.github.com>
2021-01-16 16:13:07 +0000

    correção do layout de juntar a jogo

--- PolyDraw/app/src/main/java/com/example/polydraw/PerfilActivity.kt
+++ PolyDraw/app/src/main/java/com/example/polydraw/PerfilActivity.kt
@@ -2,10 +2,15 @@ package com.example.polydraw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+    }
+
+    fun onVoltar(view: View) {
+        finish()
     }
 }
