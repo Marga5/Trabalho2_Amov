27601b9a3d1d67356755fd3fccd9cff0824bfec3
Marcos Gaspar <Marga5@users.noreply.github.com>
2021-01-16 23:29:59 +0000

    vistas
    criar partida
    entrar na partida
    espera

--- PolyDraw/app/src/main/java/com/example/polydraw/CriarActivity.kt
+++ PolyDraw/app/src/main/java/com/example/polydraw/CriarActivity.kt
@@ -5,20 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class EntrarActivity : AppCompatActivity() {
+class CriarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entrar)
+        setContentView(R.layout.activity_criar)
     }
 
     fun onVoltar(view: View) {
         finish()
     }
-    fun onCriar(view: View) {
-        val intent = Intent (this, CriarActivity::class.java)
-        startActivity(intent)
-    }
-    fun onJuntar(view: View) {
 
+    fun onCriar(view: View) {
+        val intent = Intent (this, EsperaActivity::class.java)
+        startActivity(intent)
     }
 }
